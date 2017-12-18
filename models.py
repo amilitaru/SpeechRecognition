a130c546d7999e34b291b9f40a03948cc0b408cc
ucdmilitaru <ioan-alexandru.militaru@ucdconnect.ie>
2017-12-18 18:52:38 +0700

    Tab issues for new embedded function

--- models.py
+++ models.py
@@ -980,31 +980,31 @@ def ds_cnn_large_dropout(fingerprint_input, model_settings, is_training,scope='d
   else:
     first_dropout = first_relu
 
-  def _create_ds_conv(input, is_training, downsample=False, scope):
-
-    _stride = 2 if downsample else 1
-    #he_init = tf.layers.variance_scaling_initializer() #maybe add this in a future version 
-    depthwise_conv = tf.layers.separable_conv2d(bn, 
-                                           filters=276, 
-                                           kernel_size=[3,3],
-                                           strides=[_stride,_stride],
-                                           padding='SAME', 
-                                           activation=tf.nn.relu)
-    bn = tf.layers.batch_normalization(output)
-    
-    """
-    if is_training:
-      _dropout = tf.nn.dropout(bn, dropout_prob)
-    else:
-      _dropout = bn
-    """  
-    point_conv =  tf.layers.conv2d(bn, 
-                               filters=276,
-                               kernel_size=[1,1],
-                               padding='SAME',
-                               activation=tf.nn.relu)
-    
-    return tf.layers.batch_normalization(point_conv)
+    def _create_ds_conv(input, is_training, downsample=False, scope):
+  
+      _stride = 2 if downsample else 1
+      #he_init = tf.layers.variance_scaling_initializer() #maybe add this in a future version 
+      depthwise_conv = tf.layers.separable_conv2d(bn, 
+                                             filters=276, 
+                                             kernel_size=[3,3],
+                                             strides=[_stride,_stride],
+                                             padding='SAME', 
+                                             activation=tf.nn.relu)
+      bn = tf.layers.batch_normalization(output)
+      
+      """
+      if is_training:
+        _dropout = tf.nn.dropout(bn, dropout_prob)
+      else:
+        _dropout = bn
+      """  
+      point_conv =  tf.layers.conv2d(bn, 
+                                 filters=276,
+                                 kernel_size=[1,1],
+                                 padding='SAME',
+                                 activation=tf.nn.relu)
+      
+      return tf.layers.batch_normalization(point_conv)
     
     
   second_conv = _create_ds_conv(first_dropout, is_training, downsample=True, scope)
