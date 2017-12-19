0478c00f2c6093779c7fcfd2a1f94fa37c68ceed
ucdmilitaru <ioan-alexandru.militaru@ucdconnect.ie>
2017-12-19 22:41:41 +0700

    bugfix: Imports were causing an error with train.py

--- models.py
+++ models.py
@@ -12,12 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from _tracemalloc import is_tracing
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-
 """Model definitions for simple speech recognition.
 
 """
